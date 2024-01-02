6b368a8cf63711f02a38116f7a9a0dbacb218aed
Adil Azzaoui <adilazzaoui96@gmail.com>
2024-01-02 21:04:13 +0100

    correction de bug sur la fonction add

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -4,6 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
 System.out.println(Calculatrice.multiplication(2, 2));
-
+System.out.println(Calculatrice.add(3 , 10));
 	}
 }
